53975ff07c22cc09bce77c83c48c12abd99fd1ee
Art <absolutus11@gmail.com>
2023-04-20 04:06:51 +0400

    fix: Header and Footer converted to functional components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,19 @@
-/* eslint-disable no-nested-ternary */
-/* eslint-disable react/prefer-stateless-function */
-import { Component } from 'react';
-
+import React, { ReactElement } from 'react';
 import navi from 'assets/icons/navi.svg';
 import mail from 'assets/icons/mail.svg';
 import phone from 'assets/icons/phone.svg';
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer>
-        <img src={navi} alt={navi} />
-        <p>Halabyan 2a, Yerevan, Armenia</p>
-        <img src={phone} alt={phone} />
-        <p>eutumocc@tumo.org</p>
-        <img src={mail} alt={mail} />
-        <p>+374 10 398413</p>
-      </footer>
-    );
-  }
-}
+const Footer: React.FC<object> = (): ReactElement => {
+  return (
+    <footer>
+      <img src={navi} alt={navi} />
+      <p>Halabyan 2a, Yerevan, Armenia</p>
+      <img src={phone} alt={phone} />
+      <p>eutumocc@tumo.org</p>
+      <img src={mail} alt={mail} />
+      <p>+374 10 398413</p>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,52 +1,48 @@
-/* eslint-disable no-nested-ternary */
-/* eslint-disable react/prefer-stateless-function */
-import { Component } from 'react';
+import React, { ReactElement } from 'react';
 
 import logo from 'assets/images/TUMO-Labs_logo.svg';
 import Menu from './Menu';
 
 import 'styles/Header.css';
 
-class Header extends Component {
-  menuLinks = [
-    {
-      label: 'Home',
-      to: '/',
-    },
-    {
-      label: 'Stations',
-      to: '/stations',
-    },
-    {
-      label: 'Station@Yerevan',
-      to: '/stations/yerevan',
-    },
-    {
-      label: 'Station@Dilijan',
-      to: '/stations/dilijan',
-    },
-    {
-      label: 'Projects',
-      to: '/projects',
-    },
-    {
-      label: 'TUMO boxes',
-      to: '/boxes',
-    },
-    {
-      label: 'Contact us',
-      to: '/contacts',
-    },
-  ];
+const menuLinks = [
+  {
+    label: 'Home',
+    to: '/',
+  },
+  {
+    label: 'Stations',
+    to: '/stations',
+  },
+  {
+    label: 'Station@Yerevan',
+    to: '/stations/yerevan',
+  },
+  {
+    label: 'Station@Dilijan',
+    to: '/stations/dilijan',
+  },
+  {
+    label: 'Projects',
+    to: '/projects',
+  },
+  {
+    label: 'TUMO boxes',
+    to: '/boxes',
+  },
+  {
+    label: 'Contact us',
+    to: '/contacts',
+  },
+];
 
-  render() {
-    return (
-      <header>
-        <img className="logo icon" src={logo} alt={logo} />
-        <Menu items={this.menuLinks} />
-      </header>
-    );
-  }
-}
+const Header: React.FC<object> = (): ReactElement => {
+  return (
+    <header>
+      <img className="logo icon" src={logo} alt={logo} />
+      <Menu items={menuLinks} />
+    </header>
+  );
+};
 
 export default Header;
